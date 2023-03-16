902c943a1a0bc7908d78da22deeee7ca8c6f4235
Yulunyeh <Yulunyeh@hotmail.com>
2023-03-16 11:18:03 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
 import os
-import openai
-import json
 from datetime import datetime
 
 from flask import Flask, abort, request
@@ -12,45 +10,31 @@ from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
 app = Flask(__name__)
 
-# line_bot_api = LineBotApi(os.environ.get("CHANNEL_ACCESS_TOKEN"))
-# handler = WebhookHandler(os.environ.get("CHANNEL_SECRET"))
+line_bot_api = LineBotApi(os.environ.get("CHANNEL_ACCESS_TOKEN"))
+handler = WebhookHandler(os.environ.get("CHANNEL_SECRET"))
 
 
 @app.route("/", methods=["GET", "POST"])
-def linebot():
-    body = request.get_data(as_text=True)
-    json_data = json.loads(body)
-    print(json_data)
-    try:
-        line_bot_api = LineBotApi(os.environ.get("CHANNEL_ACCESS_TOKEN"))
-        handler = WebhookHandler(os.environ.get("CHANNEL_SECRET"))
+def callback():
 
-        signature = request.headers['X-Line-Signature']
-        handler.handle(body, signature)
-        tk = json_data['events'][0]['replyToken']
-        msg = json_data['events'][0]['message']['text']
-        # 取出文字的前五個字元，轉換成小寫
-        ai_msg = msg[:6].lower()
-        reply_msg = ''
-        # 取出文字的前五個字元是 hi ai:
-        if ai_msg == 'hi ai:':
-            openai.api_key = '你的 OpenAI API Key'
-            # 將第六個字元之後的訊息發送給 OpenAI
-            response = openai.Completion.create(
-                model='text-davinci-003',
-                prompt=msg[6:],
-                max_tokens=256,
-                temperature=0.5,
-                )
-            # 接收到回覆訊息後，移除換行符號
-            reply_msg = response["choices"][0]["text"].replace('\n','')
-        else:
-            reply_msg = msg
-        text_message = TextSendMessage(text=reply_msg)
-        line_bot_api.reply_message(tk,text_message)
-    except:
-        print('error')
-    return 'OK'
+    if request.method == "GET":
+        return "Hello Heroku"
+    if request.method == "POST":
+        signature = request.headers["X-Line-Signature"]
+        body = request.get_data(as_text=True)
+
+        try:
+            handler.handle(body, signature)
+        except InvalidSignatureError:
+            abort(400)
+
+        return "OK"
 
 
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    get_message = event.message.text
 
+    # Send To Line
+    reply = TextSendMessage(text=f"{get_message}")
+    line_bot_api.reply_message(event.reply_token, reply)
